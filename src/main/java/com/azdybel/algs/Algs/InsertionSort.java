5bf2ad0e96c856aba9c7ecb3d2664a8cecf68dab
Krzysztof Kmiecik <krzysztof.kmiecik@wp.pl>
2019-05-20 00:04:57 +0200

    zrobione insertionSort

--- src/main/java/com/azdybel/algs/Algs/InsertionSort.java
+++ src/main/java/com/azdybel/algs/Algs/InsertionSort.java
@@ -3,12 +3,11 @@ package com.azdybel.algs.Algs;
 import com.azdybel.algs.Interfaces.IAlgorithmRunner;
 import com.azdybel.algs.Interfaces.IInsertionSort;
 
-import java.util.Random;
-
 public class InsertionSort implements IInsertionSort<Integer>, IAlgorithmRunner {
 
     private Integer[] table;
     private int sortedSize;
+
     @Override
     public int getSortedSize() {
         return sortedSize;
@@ -26,19 +25,48 @@ public class InsertionSort implements IInsertionSort<Integer>, IAlgorithmRunner 
         //pętla, która stworzy przestrzeń dla naszego elementu
         //wstawiamy w wolne miejsce elementToInsert
 
+        System.out.println("test_insertElementAt");
+
+        for (int i = table.length - 1; i > index; i--) {
+            table[i] = table[i - 1];
+
+        }
+        table[index] = elementToInsert;
+
     }
 
     @Override
     public void sort() {
 
+
+        int temp;
+        for (int i = 1; i < table.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (table[j] < table[j - 1]) {
+                    temp = table[j];
+                    table[j] = table[j - 1];
+                    table[j - 1] = temp;
+                }
+            }
+        }
+        for (int i : this.table) {
+            System.out.println(i);
+        }
     }
 
     @Override
     public void setup() {
-        this.table = new Integer[50];
-        for(int i=0; i<50; i++){
-            this.table[i] = new Random(System.nanoTime()).nextInt();
-        }
+        this.table = new Integer[5];
+        this.table[0] = 4;
+        this.table[1] = 3;
+        this.table[2] = 2;
+        this.table[3] = 5;
+        this.table[4] = 7;
+
+//        for(int i=0; i<5; i++){
+//            this.table[i] = new Random(System.nanoTime()).nextInt();
+//        }
+
 
     }
 
--- src/main/java/com/azdybel/algs/Main/Main.java
+++ src/main/java/com/azdybel/algs/Main/Main.java
@@ -3,6 +3,7 @@ package com.azdybel.algs.Main;
 import com.azdybel.algs.Algs.InsertionSort;
 import com.azdybel.algs.Algs.ListInsertionSort;
 import com.azdybel.algs.Algs.SimpleAlgorithm;
+import com.azdybel.algs.Helpers.TimeMeasurementHelper;
 import com.azdybel.algs.Interfaces.IAlgorithmRunner;
 
 import static com.azdybel.algs.Helpers.TimeMeasurementHelper.Compare;
@@ -10,8 +11,13 @@ import static com.azdybel.algs.Helpers.TimeMeasurementHelper.ExecutionTimeMeasur
 
 public class Main {
     public static void main(String[] args) {
-        ListInsertionSort myListIn = new ListInsertionSort();
-        myListIn.setup();
+//        ListInsertionSort myListIn = new ListInsertionSort();
+////        myListIn.setup();
+        InsertionSort insertionSort=new InsertionSort();
+       // TimeMeasurementHelper.ExecutionTimeMeasure(listInsertionSort,true);
+        insertionSort.setup();
+        insertionSort.insertElementAt(2,1);
+        insertionSort.sort();
     }
 
 }
